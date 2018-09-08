9ed75ebe7bae0a6cc66eaffd65ada9adde4bbc87
Arthur Pons <33666394+unguest@users.noreply.github.com>
2018-09-08 22:35:11 +0200

    Add files via upload

---
+++ korrect.py
@@ -0,0 +1,23 @@
+#coding: utf-8
+#@Author: Arthur PONS -> github.com/unguest
+import os
+
+def levDist(str1, str2):
+	lDist = 0
+	for i in (range(min(len(str1), len(str2)))):
+		if str1[i] != str2[i]:
+			lDist += 1
+	lDist += len(set(str1) - set(str2))
+	return lDist
+
+word = input("Entrez un mot : ")
+while len(word) = 0:
+	word = input("Entrez un mot : ")
+
+with open("dico.txt", "r") as f:
+	for word1 in f:
+		dist = levDist(word, word1)
+		if dist <= len(word) / 4:
+			print("Would you either say {} ?".format(word1))
+		else:
+			continue
